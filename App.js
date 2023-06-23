1cbf58eef3ab4abf4d326477eb306be38206c310
jarenl0ke <jarenloke1@gmail.com>
2023-06-23 11:03:26 +0800

    Add header

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./screens/Login";
+import Login from "./src/screens/Login";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 const Login = () => {
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View>
-      <Text>This is the home screen</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <View style={styles.container}>
+        <Text style={styles.header}>NUS Pasar Malam</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#121212",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  header: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 28,
+    textAlign: "center",
+    marginBottom: 30,
+  },
+});
 
 export default Login;
