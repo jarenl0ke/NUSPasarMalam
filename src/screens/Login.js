844454248829abaec292b20345ceed31ee1a7ffd
jarenl0ke <jarenloke1@gmail.com>
2023-06-23 11:18:24 +0800

    Add Login Interface

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  StyleSheet,
   TouchableWithoutFeedback,
   View,
   Text,
   Keyboard,
   Image,
-  StyleSheet,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
 
 const Login = () => {
   const dismissKeyboard = () => {
     Keyboard.dismiss();
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (email.trim() === "" || password.trim() === "") {
+      alert("Please enter your email and password");
+      return;
+    }
   };
 
   return (
@@ -21,6 +33,28 @@ const Login = () => {
           source={require("../../assets/marketplace.png")}
           style={styles.logo}
         />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          placeholderTextColor="#777"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          placeholderTextColor="#777"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.signupButton} onPress={handleLogin}>
+          <Text style={styles.signupButtonText}>Create a new account</Text>
+        </TouchableOpacity>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -45,6 +79,40 @@ const styles = StyleSheet.create({
     height: 150,
     width: 150,
     marginBottom: 30,
+  },
+  input: {
+    color: "#FFFFFF",
+    backgroundColor: "#1E1E1E",
+    fontSize: 16,
+    height: 50,
+    width: "100%",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    height: 50,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  signupButton: {
+    paddingTop: 10,
+    marginTop: 10,
+  },
+  signupButtonText: {
+    color: "#007bff",
+    fontSize: 16,
   },
 });
 
