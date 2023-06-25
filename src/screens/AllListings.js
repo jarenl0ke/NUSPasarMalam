10ff19eb48079929590946d8332f286edc881be7
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 21:59:48 +0800

    Create All Listings Page

--- src/screens/AllListings.js
+++ src/screens/AllListings.js
@@ -1,4 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
+} from "react-native";
+import firebase from "../firebase";
+import "firebase/auth";
+import { Ionicons } from "@expo/vector-icons";
+import ModalDropdown from "react-native-modal-dropdown";
 
 const AllListings = ({ navigation }) => {
   return;
