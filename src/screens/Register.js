29e63098816aa2e261af86eecc5feb729eb5a343
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 23:26:37 +0800

    Add Back Navigation Button

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -15,16 +15,44 @@ import "firebase/auth";
 import "firebase/firestore";
 import { Ionicons } from "@expo/vector-icons";
 
-const Register = () => {
+const Register = ({ navigation }) => {
   const dismissKeyboard = () => {
     Keyboard.dismiss();
   };
 
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
   return (
-    <TouchableWithoutFeedback
-      onPress={dismissKeyboard}
-    ></TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          <TouchableOpacity style={styles.goBackButton} onPress={handleGoBack}>
+            <Ionicons name="arrow-back" size={24} color="#FFFFFF" />
+          </TouchableOpacity>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    paddingBottom: 100, // Adjust the padding to leave space for the floating button
+  },
+  goBackButton: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+    zIndex: 1,
+    padding: 10,
+  },
+});
 
 export default Register;
