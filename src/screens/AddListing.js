86c339f42838bcee37288c4635d2f01b388ad96b
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 15:31:14 +0800

    Add categories

--- src/screens/AddListing.js
+++ src/screens/AddListing.js
@@ -1,4 +1,45 @@
 import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import ModalDropdown from "react-native-modal-dropdown";
+import * as ImagePicker from "expo-image-picker";
+import firebase from "../../database/Firebase";
+import "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const categories = [
+  "Electronics",
+  "Clothing & Accessories",
+  "Home & Kitchen",
+  "Furniture",
+  "Books & Magazines",
+  "Sports & Outdoors",
+  "Beauty & Personal Care",
+  "Toys & Games",
+  "Health & Fitness",
+  "Jewelry & Watches",
+  "Automotive",
+  "Baby & Kids",
+  "Art & Collectibles",
+  "Musical Instruments",
+  "Pet Supplies",
+  "Tools & Home Improvement",
+  "Office Supplies",
+  "Garden & Outdoor",
+  "Food & Beverages",
+];
 
 const AddListing = () => {
   return;
