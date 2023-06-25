57a7817128aec4519874b0b622a20fb9744d6fa5
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 22:00:20 +0800

    Add Categories

--- src/screens/AllListings.js
+++ src/screens/AllListings.js
@@ -14,6 +14,29 @@ import "firebase/auth";
 import { Ionicons } from "@expo/vector-icons";
 import ModalDropdown from "react-native-modal-dropdown";
 
+const categories = [
+  "All Categories",
+  "Electronics",
+  "Clothing & Accessories",
+  "Home & Kitchen",
+  "Furniture",
+  "Books & Magazines",
+  "Sports & Outdoors",
+  "Beauty & Personal Care",
+  "Toys & Games",
+  "Health & Fitness",
+  "Jewelry & Watches",
+  "Automotive",
+  "Baby & Kids",
+  "Art & Collectibles",
+  "Musical Instruments",
+  "Pet Supplies",
+  "Tools & Home Improvement",
+  "Office Supplies",
+  "Garden & Outdoor",
+  "Food & Beverages",
+];
+
 const AllListings = ({ navigation }) => {
   return;
 };
