dd0bb5ad1f739a16568a28e23d331421ae72bd95
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 22:49:48 +0800

    Add back navigation button

--- src/screens/MyListings.js
+++ src/screens/MyListings.js
@@ -1,7 +1,55 @@
 import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  RefreshControl,
+  ActivityIndicator,
+} from "react-native";
+import firebase from "../../database/Firebase";
+import "firebase/auth";
+import { Ionicons } from "@expo/vector-icons";
 
 const MyListings = () => {
-  return;
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.goBackButton} onPress={handleGoBack}>
+        <Ionicons name="arrow-back" size={24} color="#FFFFFF" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+    paddingHorizontal: 20,
+    paddingBottom: 40,
+  },
+  goBackButton: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+    zIndex: 1,
+    padding: 10,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+});
 
 export default MyListings;
