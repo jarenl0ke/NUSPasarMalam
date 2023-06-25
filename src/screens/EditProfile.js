ab7466431f2747ac99743206d80bbc9c5efccea7
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 22:30:26 +0800

    Add Header for Edit Profile Screen

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,7 +1,52 @@
 import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Keyboard,
+  Alert,
+} from "react-native";
+import firebase from "../../database/Firebase";
+import "firebase/firestore";
+import { Ionicons } from "@expo/vector-icons";
 
-const EditProfile = () => {
-  return;
+const EditProfile = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.goBackButton}
+        onPress={() => navigation.goBack()}
+      >
+        <Ionicons name="arrow-back" size={24} color="#FFFFFF" />
+      </TouchableOpacity>
+      <Text style={styles.header}>Edit Profile</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+  },
+  goBackButton: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+    zIndex: 1,
+    padding: 10,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 40,
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+});
 
 export default EditProfile;
