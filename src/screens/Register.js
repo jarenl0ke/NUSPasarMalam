46abb299de27998679dbf07c5fd6922afa7eeaff
jarenl0ke <jarenloke1@gmail.com>
2023-06-25 23:23:46 +0800

    Initialise Register Page

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
+import firebase from "../../database/Firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import { Ionicons } from "@expo/vector-icons";
 
 const Register = () => {
-  return;
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={dismissKeyboard}
+    ></TouchableWithoutFeedback>
+  );
 };
 
 export default Register;
