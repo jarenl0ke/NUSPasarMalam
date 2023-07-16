221a25558c6deadcb3cfa063da8a017d28ef8d5e
jarenl0ke <jarenloke1@gmail.com>
2023-07-16 16:14:16 +0800

    Abstracted Categories

---
+++ constants/Categories.js
@@ -0,0 +1,23 @@
+const Categories = [
+  "Art & Collectibles",
+  "Automotive",
+  "Baby & Kids",
+  "Beauty & Personal Care",
+  "Books & Magazines",
+  "Clothing & Accessories",
+  "Electronics",
+  "Home & Kitchen",
+  "Food & Beverages",
+  "Furniture",
+  "Garden & Outdoor",
+  "Health & Fitness",
+  "Jewelry & Watches",
+  "Musical Instruments",
+  "Office Supplies",
+  "Pet Supplies",
+  "Sports & Outdoors",
+  "Tools & Home Improvement",
+  "Toys & Games",
+];
+
+export default Categories;
