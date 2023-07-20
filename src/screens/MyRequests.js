6dcfc8cc9981d69df618a8074dea60183dff133a
jarenl0ke <jarenloke1@gmail.com>
2023-07-20 13:35:36 +0800

    Add refresh functionality

--- src/screens/MyRequests.js
+++ src/screens/MyRequests.js
@@ -1,10 +1,53 @@
-import React from "react";
-import { SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+  RefreshControl,
+  StyleSheet,
+} from "react-native";
+import firebase from "../../database/Firebase";
+import "firebase/auth";
 import { Ionicons } from "@expo/vector-icons";
 
 const MyRequests = ({ navigation }) => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  const fetchRequests = async () => {
+    try {
+      const user = firebase.auth().currentUser;
+      const requestsRef = firebase.firestore().collection("Requests");
+      const querySnapshot = await requestsRef
+        .where("userID", "==", user.uid)
+        .get();
+      const requestsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setListings(requestsData);
+    } catch (error) {
+      console.error("Error fetching listings: ", error);
+    }
+  };
+
   const handleGoBack = () => {
     navigation.goBack();
+  };
+
+  const refreshHandler = () => {
+    setRefreshing(true);
+    fetchRequests()
+      .then(() => setRefreshing(false))
+      .catch((error) => {
+        console.error("Error refreshing listings: ", error);
+        setRefreshing(false);
+      });
   };
 
   return (
@@ -12,6 +55,18 @@ const MyRequests = ({ navigation }) => {
       <TouchableOpacity style={styles.goBackButton} onPress={handleGoBack}>
         <Ionicons name="arrow-back" size={24} color="#FFFFFF" />
       </TouchableOpacity>
+      <Text style={styles.header}>My Requests</Text>
+      <ScrollView
+        style={styles.listingsContainer}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={refreshHandler}
+            colors={["#FFFFFF"]}
+            tintColor={"#FFFFFF"}
+          />
+        }
+      ></ScrollView>
     </SafeAreaView>
   );
 };
@@ -31,5 +86,15 @@ const styles = StyleSheet.create({
     left: 20,
     zIndex: 1,
     padding: 10,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+  listingsContainer: {
+    flex: 1,
   },
 });
