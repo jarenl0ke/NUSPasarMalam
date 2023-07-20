470e1bb308cad1e51ea6ebb5e249e4e7d7186a2b
jarenl0ke <jarenloke1@gmail.com>
2023-07-20 23:21:21 +0800

    Changed status bar style

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
 import Home from "./src/screens/Home";
@@ -23,92 +25,95 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ gestureEnabled: false }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Settings"
-          component={Settings}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="EditProfile"
-          component={EditProfile}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MyListings"
-          component={MyListings}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MyRequests"
-          component={MyRequests}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="AddListing"
-          component={AddListing}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="AddRequest"
-          component={AddRequest}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="AllListings"
-          component={AllListings}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="AllRequests"
-          component={AllRequests}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Listing"
-          component={Listing}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Request"
-          component={Request}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MyChats"
-          component={MyChats}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SellerChat"
-          component={SellerChat}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Settings"
+            component={Settings}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="EditProfile"
+            component={EditProfile}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="MyListings"
+            component={MyListings}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="MyRequests"
+            component={MyRequests}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="AddListing"
+            component={AddListing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="AddRequest"
+            component={AddRequest}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="AllListings"
+            component={AllListings}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="AllRequests"
+            component={AllRequests}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Listing"
+            component={Listing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Request"
+            component={Request}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="MyChats"
+            component={MyChats}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SellerChat"
+            component={SellerChat}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
